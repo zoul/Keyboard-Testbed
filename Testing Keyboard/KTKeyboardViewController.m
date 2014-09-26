e704079f89b3be83e8aa5c75d6662cfce48bf891
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-09-26 13:55:08 +0200

    Added a button to switch to next input mode.

--- Testing Keyboard/KTKeyboardViewController.m
+++ Testing Keyboard/KTKeyboardViewController.m
@@ -1,7 +1,8 @@
 #import "KTKeyboardViewController.h"
 
 @interface KTKeyboardViewController ()
-@property(strong) UIButton *button;
+@property(strong) UIButton *actionButton;
+@property(strong) UIButton *keyboardSwitchButton;
 @end
 
 @implementation KTKeyboardViewController
@@ -9,18 +10,36 @@
 - (void) viewDidLoad
 {
     [super viewDidLoad];
-    [self setButton:[[UIButton alloc] init]];
-    [_button setTitle:@"Do Something" forState:UIControlStateNormal];
-    [_button setTitleColor:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0] forState:UIControlStateNormal];
-    [_button addTarget:self action:@selector(doSomething) forControlEvents:UIControlEventTouchUpInside];
-    [_button sizeToFit];
-    [[self inputView] addSubview:_button];
+
+    UIColor *standardIOSBlue = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
+
+    [self setActionButton:[[UIButton alloc] init]];
+    [_actionButton setTitle:@"Do Something" forState:UIControlStateNormal];
+    [_actionButton setTitleColor:standardIOSBlue forState:UIControlStateNormal];
+    [_actionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    [_actionButton addTarget:self action:@selector(doSomething) forControlEvents:UIControlEventTouchUpInside];
+    [_actionButton sizeToFit];
+
+    [self setKeyboardSwitchButton:[[UIButton alloc] init]];
+    [_keyboardSwitchButton setTitle:@"Switch" forState:UIControlStateNormal];
+    [_keyboardSwitchButton setTitleColor:standardIOSBlue forState:UIControlStateNormal];
+    [_keyboardSwitchButton setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    [_keyboardSwitchButton addTarget:self action:@selector(advanceToNextInputMode) forControlEvents:UIControlEventTouchUpInside];
+    [_keyboardSwitchButton sizeToFit];
+
+    [[self inputView] addSubview:_actionButton];
+    [[self inputView] addSubview:_keyboardSwitchButton];
 }
 
 - (void) viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
-    [_button setCenter:[[self inputView] center]];
+    [_actionButton setCenter:[[self inputView] center]];
+    CGRect containerFrame = [[self inputView] frame];
+    CGRect kbSwitchFrame = [_keyboardSwitchButton frame];
+    [_keyboardSwitchButton setCenter:CGPointMake(
+        CGRectGetMaxX(containerFrame)-CGRectGetWidth(kbSwitchFrame)/2-10,
+        CGRectGetMaxY(containerFrame)-CGRectGetHeight(kbSwitchFrame)/2-10)];
 }
 
 - (void) doSomething
